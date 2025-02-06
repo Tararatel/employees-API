0a195bfc106dcad4c92cab71703554d197b81828
Tararatel <caratel18@mail.ru>
2025-02-06 10:20:33 +0300

    add facker-js

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,23 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+  });
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+
+  app.useStaticAssets(join(__dirname, 'public'));
+  app.useStaticAssets(join(__dirname, 'images'), { prefix: '/images' });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
